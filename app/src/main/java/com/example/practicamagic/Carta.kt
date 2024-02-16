d00a379d2b6b94f2c0ec4f14070b0ebd2c933a24
AngelViedma <19angelviedma@gmail.com>
2024-02-16 16:12:40 +0100

    creada clase carta

---
+++ app/src/main/java/com/example/practicamagic/Carta.kt
@@ -0,0 +1,15 @@
+package com.example.practicamagic
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Carta(
+    var nombre : String? = null,
+    var categoria: String? = null,
+    var precio: Double? = null,
+    var stock: Boolean? = null,
+    var imagen: String? = null,
+):Parcelable
+
+
